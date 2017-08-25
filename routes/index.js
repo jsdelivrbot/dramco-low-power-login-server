624d364906118ef927f836dc7ae0c9988617825e
GillesCallebaut <callebaut.gilles@gmail.com>
2017-08-25 10:42:11 +0200

    resolved no file issue when reading

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var request = require('request');// You can include your JSON data directly with json parameter with request library
 var fs = require("fs");
+var path = require("path");
 var router = express.Router();
+
+const PATH = path.join(__dirname, "data.json");
 
 // validate received key
 // more info: https://stackoverflow.com/questions/42333598/simple-nodejs-http-request-equivalent-for-curl
 function validateRequest(applicationID, key, callback) {
     var options = {
-        uri: 'https://'+applicationID+'.data.thethingsnetwork.org/api/v2/query?last=7d',
+        uri: 'https://' + applicationID + '.data.thethingsnetwork.org/api/v2/query?last=7d',
         method: 'GET',
         headers: {
-            'Authorization': 'key '+key,
+            'Authorization': 'key ' + key,
             'Accept': 'application/json'
         }
     };
 
-     var req = request(options, function(error, response, body) {
-         console.log("receive status code : " + response.statusCode);
-         if (response.statusCode === 200) {
-             console.log(body);
-             callback(body);
-             return;
-         }
+    var req = request(options, function (error, response, body) {
+        console.log("receive status code : " + response.statusCode);
+        if (response.statusCode === 200) {
+            console.log(body);
+            callback(body);
+            return;
+        }
         if (error) {
             console.log(error);
         }
@@ -39,66 +42,73 @@ function containsObject(obj, list) {
 
     return false;
 }
+function checkForFile(fileName, callback) {
+    fs.stat(PATH, function(err, stat) {
+        if(err == null) {
+            console.log('File exists');
+        } else if(err.code == 'ENOENT') {
+            // file does not exist
+            fs.writeFile(PATH, '');
+        } else {
+            console.log('Some other error: ', err.code);
+        }
+        callback();
+    });
+}
 
-function addRegistrationToFile(applicationID,key) {
-        fs.readFile("data.json", 'utf8', function (err, data) {
-            if (err.code === 'ENOENT') {
-                console.log('File not found!');
-                // no problem
-            } else {
-                throw err;
+function getRegistrationsFromFile(callback) {
+    checkForFile(PATH, function () {
+        fs.readFile(PATH, 'utf8', function (err, data) {
+            if (err) throw err;
+            if(data){
+                data = JSON.parse(data);
+            }else{
+                data = [];
             }
-
-            var newRegistrationData = {applicationID: applicationID, key: key};
-            var oldRegistrationData = [];
-            if (data instanceof Array) {
-                oldRegistrationData = data;
-            }
-            //only store new registration if it is a new one
-            if(!containsObject(newRegistrationData,oldRegistrationData)) oldRegistrationData.push(newRegistrationData);
-            fs.writeFile ("data.json", JSON.stringify(oldRegistrationData), function(err) {
-                if (err) throw err;
-                console.log('complete');
-            });
+            console.log("Read: "+data);
+            callback(data);
         });
+    });
+}
+
+function addRegistrationToFile(applicationID, key) {
+    getRegistrationsFromFile(function (oldRegistrationData) {
+        var newRegistrationData = {applicationID: applicationID, key: key};
+        //only store new registration if it is a new one
+        if (!containsObject(newRegistrationData, oldRegistrationData)) oldRegistrationData.push(newRegistrationData);
+        fs.writeFile(PATH, JSON.stringify(oldRegistrationData), function (err) {
+            if (err) throw err;
+            console.log('complete');
+        });
+    });
 }
 
 /* GET home page. */
 // https://low-power-login.herokuapp.com/login?applicationID=dramco-low-power-sensor-tutorial&key=ttn-account-v2.5VvYm0qTZgr1tBpL-FmRq1XDW5mV_uo3HwR5rVp09HM
-router.get('/login', function(req, res) {
-  var applicationID = req.query.applicationID;
-  var key = req.query.key;
+router.get('/login', function (req, res) {
+    var applicationID = req.query.applicationID;
+    var key = req.query.key;
 
-    validateRequest(applicationID, key, function(responseBody){
+    validateRequest(applicationID, key, function (responseBody) {
         var content = '';
-        if(responseBody){
+        if (responseBody) {
             content = 'Success!';
-            addRegistrationToFile(applicationID,key);
-        }else{
+            addRegistrationToFile(applicationID, key);
+        } else {
             content = 'Something went wrong. Are you sure you have correctly configured your Data Storage integration?';
         }
-        res.render('index', { title: 'Login' , content: content, body : responseBody});
+        res.render('index', {title: 'Login', content: content, body: responseBody});
     });
 });
 
 
 // Request user data
-router.get('/users', function(req,res){
-    fs.readFile("data.json", 'utf8', function (err, data) {
-        if (err.code === 'ENOENT') {
-            console.log('File not found!');
-            // no problem
-        } else {
-            throw err;
-        }
+router.get('/users', function (req, res) {
 
-        var registrationData = [];
-        if (data instanceof Array) {
-            registrationData = data;
-        }
+    getRegistrationsFromFile(function(registrationData){
         res.send(JSON.stringify(registrationData));
     });
-});
 
+});
 
 module.exports = router;
